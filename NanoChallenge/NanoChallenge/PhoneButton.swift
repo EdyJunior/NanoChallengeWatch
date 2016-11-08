c29ece88b000ce529fb93aa780d03b1accecf74e
Augusto Falcao <falcaoaugustos@gmail.com>
2016-11-08 17:48:53 -0300

    Adaptando a classe botão para o iPhone

--- NanoChallenge/NanoChallenge/PhoneButton.swift
+++ NanoChallenge/NanoChallenge/PhoneButton.swift
@@ -6,4 +6,59 @@
 //  Copyright © 2016 Edvaldo Junior. All rights reserved.
 //
 
-import Foundation
+import SpriteKit
+
+class PhoneButton: SKNode {
+    
+    var defaultButton: SKSpriteNode
+    var activeButton: SKSpriteNode
+    var touchableArea: SKSpriteNode
+    var action: ((_ button: PhoneButton) -> Void)?
+    var enabled: Bool = true
+    
+    init(defaultButtonImage: String, activeButtonImage: String, buttonAction: ((_ button: PhoneButton) -> Void)? = nil) {
+        
+        defaultButton = SKSpriteNode(imageNamed: defaultButtonImage)
+        activeButton = SKSpriteNode(imageNamed: activeButtonImage)
+        touchableArea = SKSpriteNode()
+        activeButton.isHidden = true
+        action = buttonAction
+        
+        super.init()
+        
+        isUserInteractionEnabled = true
+    }
+    
+    required init(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        if touchableArea.contains((touches.first?.location(in: self))!) {
+            if action != nil && enabled {
+                activeButton.isHidden = false
+                defaultButton.isHidden = true
+                action!(self)
+            }
+        }
+    }
+    
+    func setSizeAndPosition(_ size: CGSize, position: CGPoint, areaFactor factor: CGFloat) {
+        
+        defaultButton.size = size
+        activeButton.size = size
+        
+        touchableArea = SKSpriteNode()
+        touchableArea.size = CGSize(width: size.width * factor, height: size.height * factor)
+        touchableArea.position = position
+        
+                touchableArea.color = .blue
+                touchableArea.alpha = 0.5
+        
+        touchableArea.addChild(defaultButton)
+        touchableArea.addChild(activeButton)
+        addChild(touchableArea)
+    }
+    
+}

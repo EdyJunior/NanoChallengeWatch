2501f951ad7cd74ced97485b974e6753779afc3f
Augusto Falcao <falcaoaugustos@gmail.com>
2016-11-08 19:12:51 -0300

    Atualizando diagramação da tela do iPhone

--- NanoChallenge/NanoChallenge/TamaScreenScene.swift
+++ NanoChallenge/NanoChallenge/TamaScreenScene.swift
@@ -17,21 +17,14 @@ class TamaScreenScene: SKScene {
     override func sceneDidLoad() {
         
         self.lastUpdateTime = 0
-        //createTest()
         createTama()
+        createLevelLabel()
+        createExperienceLabel()
+        createStaminaLabel()
         createButtons()
         
         
         self.isUserInteractionEnabled = false
-    }
-
-    func createTest() {
-        let test = SKSpriteNode(imageNamed: "Coelho_1-0")
-        test.setScale(0.004)
-        //test.size = CGSize(width: 0.5, height: 0.5)
-        test.zPosition = 1000
-        test.position = CGPoint(x: 0.5, y: 0.5)
-        addChild(test)
     }
     
     func createTama() {
@@ -50,50 +43,72 @@ class TamaScreenScene: SKScene {
         self.addChild(bicho)
     }
     
+    func createLevelLabel() {
+        let levelLabel = SKLabelNode(text: "LVL: 12")
+        levelLabel.position = CGPoint(x: -mySize.width * 0.6, y: mySize.height * 0.75)
+        levelLabel.fontSize = mySize.height * 0.06
+        addChild(levelLabel)
+    }
+    
+    func createExperienceLabel() {
+        let experienceLabel = SKLabelNode(text: "XP: 520")
+        experienceLabel.position = CGPoint(x: 0.0, y: mySize.height * 0.75)
+        experienceLabel.fontSize = mySize.height * 0.06
+        addChild(experienceLabel)
+    }
+    
+    func createStaminaLabel() {
+        let staminaLabel = SKLabelNode(text: "STA: 04")
+        staminaLabel.position = CGPoint(x: mySize.width * 0.6, y: mySize.height * 0.75)
+        staminaLabel.fontSize = mySize.height * 0.06
+        addChild(staminaLabel)
+    }
+    
     func createButtons() {
         
-        let sSize = CGSize(width: CGFloat(1.0), height: CGFloat(1.0))
+        var sSize = CGSize(width: CGFloat(1.0), height: CGFloat(1.0))
+        sSize = mySize
         
-        let buttonsSize = CGSize(width: sSize.width * 0.1, height: sSize.width * 0.1)
+        let buttonsSize = CGSize(width: sSize.height * 0.15, height: sSize.height * 0.15)
         
-        let eatPos = CGPoint(x: sSize.width * 0.2, y: sSize.height * 0.2)
+        let eatPos = CGPoint(x: sSize.width * -0.6, y: sSize.height * -0.55)
         let eatBtn = PhoneButton(defaultButtonImage: "icon-4", activeButtonImage: "icon-4")
         eatBtn.setSizeAndPosition(buttonsSize, position: eatPos, areaFactor: 1)
         eatBtn.action = { (button: PhoneButton) -> Void in print("Edvaldo é o universo!") }
         eatBtn.action = eatAction
         addChild(eatBtn)
         
-        let bathPos = CGPoint(x: sSize.width * 0.4, y: sSize.height * 0.2)
+        let bathPos = CGPoint(x: sSize.width * -0.2, y: sSize.height * -0.55)
         let bathBtn = PhoneButton(defaultButtonImage: "icon-1", activeButtonImage: "icon-1")
         bathBtn.setSizeAndPosition(buttonsSize, position: bathPos, areaFactor: 1)
         addChild(bathBtn)
         
-        let touchPos = CGPoint(x: sSize.width * 0.6, y: sSize.height * 0.2)
+        let touchPos = CGPoint(x: sSize.width * 0.2, y: sSize.height * -0.55)
         let touchBtn = PhoneButton(defaultButtonImage: "icon-2", activeButtonImage: "icon-2")
         touchBtn.setSizeAndPosition(buttonsSize, position: touchPos, areaFactor: 1)
         addChild(touchBtn)
         
-        let curePos = CGPoint(x: sSize.width * 0.8, y: sSize.height * 0.2)
+        let curePos = CGPoint(x: sSize.width * 0.6, y: sSize.height * -0.55)
         let cureBtn = PhoneButton(defaultButtonImage: "icon-3", activeButtonImage: "icon-3")
         cureBtn.setSizeAndPosition(buttonsSize, position: curePos, areaFactor: 1)
         addChild(cureBtn)
         
-        let sleepPos = CGPoint(x: sSize.width * 0.2, y: sSize.height * 0.07)
+        let sleepPos = CGPoint(x: sSize.width * -0.6, y: sSize.height * -0.8)
         let sleepBtn = PhoneButton(defaultButtonImage: "icon-4", activeButtonImage: "icon-4")
         sleepBtn.setSizeAndPosition(buttonsSize, position: sleepPos, areaFactor: 1)
         addChild(sleepBtn)
         
-        let trainPos = CGPoint(x: sSize.width * 0.4, y: sSize.height * 0.07)
+        let trainPos = CGPoint(x: sSize.width * -0.2, y: sSize.height * -0.8)
         let trainBtn = PhoneButton(defaultButtonImage: "icon-0", activeButtonImage: "icon-0")
         trainBtn.setSizeAndPosition(buttonsSize, position: trainPos, areaFactor: 1)
         addChild(trainBtn)
         
-        let studyPos = CGPoint(x: sSize.width * 0.6, y: sSize.height * 0.07)
+        let studyPos = CGPoint(x: sSize.width * 0.2, y: sSize.height * -0.8)
         let studyBtn = PhoneButton(defaultButtonImage: "icon-1", activeButtonImage: "icon-1")
         studyBtn.setSizeAndPosition(buttonsSize, position: studyPos, areaFactor: 1)
         addChild(studyBtn)
         
-        let statusPos = CGPoint(x: sSize.width * 0.8, y: sSize.height * 0.07)
+        let statusPos = CGPoint(x: sSize.width * 0.6, y: sSize.height * -0.8)
         let statusBtn = PhoneButton(defaultButtonImage: "icon-2", activeButtonImage: "icon-2")
         statusBtn.setSizeAndPosition(buttonsSize, position: statusPos, areaFactor: 1)
         addChild(statusBtn)

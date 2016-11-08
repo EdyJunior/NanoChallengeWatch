6c24b933f691bcf85e832bb81f99bccbcff22072
padrao <padrao@macbook-pro-de-berg.local>
2016-11-08 09:38:02 -0200

    added class Tama.

---
+++ NanoChallenge/NanoChallenge/Tama.swift
@@ -0,0 +1,22 @@
+//
+//  Tama.swift
+//  NanoChallenge
+//
+//  Created by padrao on 08/11/16.
+//  Copyright © 2016 Edvaldo Junior. All rights reserved.
+//
+
+import UIKit
+
+class Tama: NSObject {
+    
+    var name: String!
+    
+    init(name: String) {
+        super.init()
+        self.name = name
+    }
+    
+    
+
+}

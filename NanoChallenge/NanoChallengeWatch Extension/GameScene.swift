4aade79e4362727ddbabe11097d749215baa069b
Augusto Falcao <falcaoaugustos@gmail.com>
2016-11-08 10:55:10 -0300

    Inserindo metodo Update no Game Scene

--- NanoChallenge/NanoChallengeWatch Extension/GameScene.swift
+++ NanoChallenge/NanoChallengeWatch Extension/GameScene.swift
@@ -52,4 +52,8 @@ class GameScene: SKScene {
         bicho.run(SKAction.repeatForever(SKAction.animate(with: frames, timePerFrame: 0.3)))
         scene!.addChild(bicho)
     }
+    
+    override func update(_ currentTime: TimeInterval) {
+        // Colocando atualização de barra e demais coisas aqui!
+    }
 }
